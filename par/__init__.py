dec2d3bd2d188957b6d9485de5137fd0545ad05b
limodou <limodou@gmail.com>
2012-10-19 22:05:31 +0800

    fix version number

--- par/__init__.py
+++ par/__init__.py
@@ -14,7 +14,7 @@ __author__ = 'limodou'
 __author_email__ = 'limodou@gmail.com'
 __url__ = 'https://github.com/limodou/par'
 __license__ = 'BSD'
-__version__ = '0.2'
+__version__ = '0.3'
 
 _ = re.compile
 
